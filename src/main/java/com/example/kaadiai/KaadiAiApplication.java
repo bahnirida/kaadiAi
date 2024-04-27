5ca8b084cc9b8e5830fa4e8605418e0dab54a401
bahnirida <bahni.rida.contact@gmail.com>
2024-04-27 11:50:58 +0100

    first commit

---
+++ src/main/java/com/example/kaadiai/KaadiAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.kaadiai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaadiAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KaadiAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kaadiai/KaadiAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kaadiai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaadiAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
