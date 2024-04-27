69944545533db60e24ab5da4a919adef5df58ae0
bahnirida <bahni.rida.contact@gmail.com>
2024-04-27 13:15:51 +0100

    first edit

---
+++ src/main/java/com/example/kaadiai/controller/ChatController.java
@@ -0,0 +1,18 @@
+package com.example.kaadiai.controller;
+
+import org.springframework.ai.chat.ChatClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChatController {
+
+    @Autowired
+    private ChatClient chatClient;
+    @GetMapping()
+    public String chat(String query){
+        String response = chatClient.call(query);
+        return response;
+    }
+}
